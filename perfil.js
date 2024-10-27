bc47ff9cb18d65c8175cc91e6c09d1d473979fed
GollinoGui <131721946+GollinoGui@users.noreply.github.com>
2024-10-27 18:52:48 -0300

    Create perfil.js

---
+++ perfil.js
@@ -0,0 +1,8 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("open");
+}
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('open'); // Adiciona ou remove a classe que controla a visibilidade
+}
